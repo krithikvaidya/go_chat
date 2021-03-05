0eb45b7125aeb1380fde274d2fb42e33a1fb16e0
Krithik Vaidya <krithikvaidya@gmail.com>
2021-03-05 19:08:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -16,13 +16,6 @@ Protocol for sending/receiving messages:
 */
 
 /**
-TODO:
-add support for utf-8 https://medium.com/rungo/string-data-type-in-go-8af2b639478
-add prompt for entering a message
-stuff labelled as todo
-check if contexts are propogated properly  -- DONE
-dockerize it  -- DONE
-allow for 1:1 communication (should be easy) -- DONE
 
 Possible extensions:
 use gorilla websockets instead of raw sockets, get a web-based frontend
